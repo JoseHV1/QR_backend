c63fe67f6c1ebffc87da96d9117777bbe53560b0
Jose Hernandez <jitprogramadores@gmail.com>
2024-06-24 18:33:33 -0400

    corrections in controller

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -1,20 +1,15 @@
 import qrcode from "qrcode";
 
 export async function generateQR(req, res) {
-    console.log(req.body);
-    
     const { url } = req.body;
-    const expRegURL = /^(http|https)\:\/\/[a-z0-9\.-]+\.[a-z]{2,4}/gi;
+    const expRegURL = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
 
     if(!url){
         res.send({message: 'Error: Url empty'});
-        return res.redirect('back');
     }
-
 
     if(!url.match(expRegURL)){
         res.send({message: 'Error: Url invalidate'});
-        return res.redirect('back');
     }
 
     await qrcode.toDataURL(url, (err, src) => {
